390477b50d5c9956e4770f9dea1608c58acb9675
Vinícios Lisboa <developerlisboa@gmail.com>
2021-10-14 15:15:46 -0300

    deploy

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://blogdolisboa.herokuapp.com/temas', this.token)
+  }
+
+  getByIdTema(id: Number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogdolisboa.herokuapp.com/temas/${id}`, this.token)
+  }
+
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`https://blogdolisboa.herokuapp.com/temas/nome/${nome}`, this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('https://blogdolisboa.herokuapp.com/temas', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogdolisboa.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogdolisboa.herokuapp.com/temas/${id}`, this.token)
+  }
+
+}
